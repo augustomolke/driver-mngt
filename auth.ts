64ad2c1021759cacb5ec78a24f4bc6e980150ced
Augusto Otto <augustomolke@gmail.com>
2024-10-12 17:08:36 -0300

    fix login deploy

--- auth.ts
+++ auth.ts
@@ -85,7 +85,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           throw new Error("Driver ID não encontrado");
         }
 
-        if (!(user.data.phone.toString().slice(-4) == password)) {
+        console.log("LOOOOOOG USEEEEER", user);
+
+        if (!(user.data.phone?.toString().slice(-4) == password)) {
           // No user found, so this is their first attempt to login
           // meaning this is also the place you could do registration
           throw new Error("Telefone incorreto");
