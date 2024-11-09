511ec5d647b1821ce142d30541a4b9cb2d63d96e
Augusto Otto <augustomolke@gmail.com>
2024-11-09 10:36:03 -0300

    error log

--- app/error.tsx
+++ app/error.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/card";
 import SignOutButton from "@/components/signout-button";
 import { CircleX } from "lucide-react";
+import { useEffect } from "react";
 
 export default function Error({
   error,
@@ -18,14 +19,18 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    console.log(error, error.message, error.cause, error.name, error.stack);
+  }, []);
   const title =
     error.message == "No locations"
       ? "Seu hub não está em operação ainda."
       : "Ops, algum erro aconteceu.";
 
-  const message = "No locations"
-    ? "Por enquanto, não existem áreas de atendimento para o seu hub. Para saber mais, entre em contato com nosso suporte oficial."
-    : "Tente novamente";
+  const message =
+    error.message == "No locations"
+      ? "Por enquanto, não existem áreas de atendimento para o seu hub. Para saber mais, entre em contato com nosso suporte oficial."
+      : "Tente novamente";
 
   return (
     <Card className="max-w-3xl mx-auto">
