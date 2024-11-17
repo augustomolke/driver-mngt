ed1b329f4d6c49fbb832b3677fa2236a92024bda
Augusto Otto <augustomolke@gmail.com>
2024-11-17 12:34:48 -0300

    fix logout

--- components/signout-button.tsx
+++ components/signout-button.tsx
@@ -1,36 +1,12 @@
-"use client";
 import { ExitIcon } from "@radix-ui/react-icons";
-import { signOut } from "next-auth/react";
-import { Button } from "./ui/button";
-import { useState } from "react";
-import { Tooltip } from "./ui/tooltip";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function SignOutButton({ text, size = "icon" }) {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSignOut = async () => {
-    setIsLoading(true);
-    try {
-      await signOut();
-    } catch (error) {
-      console.error("Error signing out:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <Tooltip content="Sign out">
-      <Button
-        onClick={handleSignOut}
-        variant="outliner"
-        size={size}
-        disabled={isLoading}
-        aria-label="Sign out"
-      >
-        {text && <span className="text-xl mr-2">{text}</span>}
-        <ExitIcon className="h-4 w-4" />
-      </Button>
-    </Tooltip>
+    <Link href={"/logout"} variant="outliner" size={size} aria-label="Sign out">
+      {text && <span className="text-xl mr-2">{text}</span>}
+      <ExitIcon className="h-4 w-4" />
+    </Link>
   );
 }
