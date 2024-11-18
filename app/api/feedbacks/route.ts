a14b2d730caec92d35d9e372d532cc18526b376f
Augusto Otto <augustomolke@gmail.com>
2024-11-18 10:29:24 -0300

    add feedbacks and preferences endpoint

--- app/api/feedbacks/route.ts
+++ app/api/feedbacks/route.ts
@@ -1,8 +1,8 @@
 import prisma from "@/lib/db/db";
 import { date } from "zod";
 
-export async function POST(request: Request) {
-  const data = await prisma.feedbacks.findMany({});
+export async function GET(request: Request) {
+  const data = await prisma.feedbacks.findMany();
 
   return Response.json({ data });
 }
--- app/api/preferences/route.ts
+++ app/api/preferences/route.ts
@@ -1,8 +1,17 @@
 import prisma from "@/lib/db/db";
 import { date } from "zod";
 
+export async function GET(request: Request) {
+  const data = await prisma.preferences.findMany();
+
+  return Response.json({ data });
+}
+
 export async function POST(request: Request) {
-  const data = await prisma.preferences.findMany({});
+  const { station } = await request.json();
+  const data = await prisma.preferences.findMany({
+    where: { station },
+  });
 
   return Response.json({ data });
 }
