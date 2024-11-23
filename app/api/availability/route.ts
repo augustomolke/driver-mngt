1c2108475d4394040962b1d9637fd1b12bc12b0d
Augusto Otto <augustomolke@gmail.com>
2024-11-23 06:55:55 -0300

    add yesterday availability

--- app/api/availability/route.ts
+++ app/api/availability/route.ts
@@ -3,8 +3,10 @@ import prisma from "@/lib/db/db";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
   const data = await prisma.bookings.findMany({
-    where: { date: { gte: new Date() }, event: { event_type: "AVAILABILITY" } },
+    where: { date: { gte: yesterday }, event: { event_type: "AVAILABILITY" } },
   });
   return Response.json({ data });
 }
