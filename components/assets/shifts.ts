0592f633fe4606c34ad2579b234417044ed2d9d9
Augusto Otto <augustomolke@gmail.com>
2025-02-25 19:12:33 -0300

    remove am

--- components/assets/shifts.ts
+++ components/assets/shifts.ts
@@ -1,5 +1,5 @@
 export const OwnFlexShifts = [
-  { id: "AM", description: "6h às 10h" }, //, limit: { h: 11, d: -1 } },
+  { id: "AM", description: "6h às 10h", limit: { h: 11, d: -1 } },
   { id: "PM", description: "15:30h às 18h", exclude: [0] },
 ];
 
